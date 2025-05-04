45d1e444d49e29a458f487a75da9fffbcceeae8b
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2025-05-04 12:53:23 +0000

    Sync LeetCode submission - Valid Anagram - Runtime - 0 ms (100.00%), Memory - 9.7 MB (50.09%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        vector<int> cnt(26, 0);
+        for (char c : s) cnt[c - 'a']++;
+        for (char c : t) cnt[c - 'a']--;
+        for (int x : cnt)
+            if (x!=0)
+                return false;
+        return true;
+    }
+};

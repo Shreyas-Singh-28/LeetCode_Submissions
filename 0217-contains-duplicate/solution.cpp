af893246af255f39b389345cd1d226800be8a5bc
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2025-05-06 18:49:47 +0000

    Sync LeetCode submission - Contains Duplicate - Runtime - 44 ms (75.45%), Memory - 91 MB (59.31%)

---
+++ 0217-contains-duplicate/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i:nums) {
+            if(s.count(i)) return 1;
+            s.insert(i);
+        }
+        return 0;
+    }
+};

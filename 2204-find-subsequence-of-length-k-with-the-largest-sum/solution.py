b0b4551f3ca12e35384b5ff119a371762b3845b8
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2025-06-29 14:41:06 +0000

    Sync LeetCode submission - Find Subsequence of Length K With the Largest Sum - Runtime - 5 ms (59.76%), Memory - 12.7 MB (17.07%)

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/solution.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/solution.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def maxSubsequence(self, nums, k):
-        result = []
-        for i in range(k):
-            result.append(nums[i])
-        for i in range(k, len(nums)):
-            min_value = min(result)
-            if nums[i] > min_value:
-                result.remove(min_value)
-                result.append(nums[i])
+        x = [(num, i) for i, num in enumerate(nums)]
+        x.sort(reverse=True)
+        temp = [(i, num) for num, i in x[:k]]
+        temp.sort()
+        result = [num for (i, num) in temp]
         return result

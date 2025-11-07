08fcc3106eb004181eb3af4bb3fe9ddc42046d11
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2025-11-07 20:27:03 +0000

    Sync LeetCode submission - Power of Three - Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- 0326-power-of-three/solution.py
+++ 0326-power-of-three/solution.py
@@ -1,12 +1,7 @@
-class Solution(object):
-    def isPowerOfThree(self, n):
-        if n <= 0:
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<=0:
             return False
-        result = True
-        while n != 1:
-            if n % 3 != 0:
-                result = False
-                break
-            else:
-                n /= 3
-        return result
+        while n%3==0:
+            n/=3
+        return n==1

d70dc8dbc7a9d3e225dd1656c38948d1d3ec4755
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2025-11-12 13:47:59 +0000

    Sync LeetCode submission - Max Consecutive Ones - Runtime - 12 ms (76.29%), Memory - 20.3 MB (100.00%)

---
+++ 0485-max-consecutive-ones/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        ans=0
+        temp=0
+        for num in nums:
+            if num==1:
+                temp+=1
+            else:
+                temp=0
+            if temp>ans:
+                ans=temp
+        return ans

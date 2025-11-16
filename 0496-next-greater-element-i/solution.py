542243e7fecdc7497c04fdf3646f93a55a18fc8e
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2025-11-16 16:38:01 +0000

    Sync LeetCode submission - Next Greater Element I - Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- 0496-next-greater-element-i/solution.py
+++ 0496-next-greater-element-i/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         dic = {}
-        for i in range(len(nums2)):
-            for j in range(i+1,len(nums2)):
-                if nums2[j]>nums2[i]:
-                    dic[nums2[i]]=nums2[j]
-                    break
-            else:
-                dic[nums2[i]]=-1
-        ans=[dic[num] for num in nums1]
+        stack = []
+        for num in nums2:
+            while stack and stack[-1] < num:
+                dic[stack[-1]] = num
+                stack.pop()
+            stack.append(num)
+        while stack:
+            dic[stack[-1]]=-1
+            stack.pop()
+        ans = [dic[num] for num in nums1]
         return ans

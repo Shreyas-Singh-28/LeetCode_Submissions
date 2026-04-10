e3eb22e4eb481f5573c371ba73d2b602aad1027b
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-04-10 05:17:42 +0000

    Sync LeetCode submission - Find Pivot Index - Runtime - 0 ms (100.00%), Memory - 35.8 MB (56.16%)

---
+++ 0724-find-pivot-index/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1;i<n;i++) nums[i]+=nums[i-1];
+        for(int i=0;i<n;i++) {
+            if(i==0) {
+                if((nums[n-1]-nums[i])==0) return i;
+            }
+            else if(i==n-1) {
+                if(nums[n-2]==0) return i;
+            }
+            else {
+                if(nums[i-1]==(nums[n-1]-nums[i]))
+                return i;
+            }
+        }
+        return -1;
+    }
+};

2b6b4769a2044a33bb4983a2fb308e3b16bbd2fb
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-04-19 17:47:05 +0000

    Sync LeetCode submission - Find All Numbers Disappeared in an Array - Runtime - 88 ms (5.17%), Memory - 65.9 MB (11.38%)

--- 0448-find-all-numbers-disappeared-in-an-array/solution.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/solution.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> freq(n+1,0);
+        set<int> s(nums.begin(),nums.end());
         vector<int> res;
-        for(auto x:nums) freq[x]=1;
-        for(int i=0;i<n;i++) if(freq[i+1]==0) res.push_back(i+1);
+        for(int i=1;i<=nums.size();i++) {
+            if(!s.count(i)) {
+                res.push_back(i);
+            }
+        }
         return res;
     }
 };

f2c80cd5968b20abeaa9b54db07dd8378dc36134
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-05-24 10:46:19 +0000

    Sync LeetCode submission - Squares of a Sorted Array - Runtime - 0 ms (100.00%), Memory - 30.3 MB (38.78%)

--- 1019-squares-of-a-sorted-array/solution.cpp
+++ 1019-squares-of-a-sorted-array/solution.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        multiset<int> s;
-        for(auto &x:nums) s.insert(x*x);
-        nums.assign(s.begin(),s.end());
-        return nums;
+        int n=nums.size();
+        int l=0,r=n-1;
+        int k=n-1;
+        vector<int> ans(n);
+        while(l<=r) {
+            if(nums[l]*nums[l]<nums[r]*nums[r]) {
+                ans[k--]=nums[r]*nums[r];
+                r--;
+            }
+            else{
+                ans[k--]=nums[l]*nums[l];
+                l++;
+            }
+        }
+        return ans;
     }
 };

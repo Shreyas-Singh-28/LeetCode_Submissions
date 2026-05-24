7c5109c3c2bf4b1095970ad972a0066c185819f1
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-05-24 18:35:17 +0000

    Sync LeetCode submission - Single Element in a Sorted Array - Runtime - 1 ms (25.45%), Memory - 52.8 MB (78.40%)

---
+++ 0540-single-element-in-a-sorted-array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1)
+        return nums[0];
+
+        int x=0;
+        for(int i=0;i<nums.length;i++){
+            x=x^nums[i];
+        }
+return x;
+
+    }
+}

3e3ef912ebc30fa31c412228023a9c8b07c0a18d
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-06-03 08:09:50 +0000

    Sync LeetCode submission - Group Anagrams - Runtime - 39 ms (17.26%), Memory - 31.7 MB (14.23%)

--- 0049-group-anagrams/solution.cpp
+++ 0049-group-anagrams/solution.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> m;
+        map<vector<int>,vector<string>> m;
         for(const auto &x:strs) {
-            string word_sorted=x;
-            sort(word_sorted.begin(),word_sorted.end());
-            m[word_sorted].push_back(x);
+            vector<int> a(26,0);
+            // string word_sorted=x;
+            // sort(word_sorted.begin(),word_sorted.end());
+            int len=x.length();
+            for(int i=0;i<len;i++) {
+                a[x[i]-'a']++;
+            }
+            m[a].push_back(x);
         }
         vector<vector<string>> res;
         for(auto &x:m) {

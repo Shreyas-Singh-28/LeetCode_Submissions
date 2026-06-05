67547f4ae73c74e0ffd5d22ca3315d4fc4a0cc85
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-06-05 06:45:10 +0000

    Sync LeetCode submission - Set Matrix Zeroes - Runtime - 0 ms (100.00%), Memory - 20.8 MB (53.42%)

--- 0073-set-matrix-zeroes/solution.cpp
+++ 0073-set-matrix-zeroes/solution.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<int> rows(m,1);
-        vector<int> cols(n,1);
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        int row0=0;
+        int col0=0;
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
                 if(matrix[i][j]==0) {
-                    rows[i]=0;
-                    cols[j]=0;
+                    if(i==0) row0=1;
+                    if(j==0) col0=1;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(rows[i]==0 || cols[j]==0) {
+        for(int i=1;i<rows;i++) {
+            for(int j=1;j<cols;j++) {
+                if(matrix[i][0]==0 || matrix[0][j]==0) {
                     matrix[i][j]=0;
+                }
+            }
+        }
+        if(row0) {
+            for(int i=0;i<1;i++) {
+                for(int j=0;j<cols;j++) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(col0) {
+            for(int i=0;i<1;i++) {
+                for(int j=0;j<rows;j++) {
+                    matrix[j][i]=0;
                 }
             }
         }

79606117aba718fd4d59991b0531ff32da766f7c
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-06-10 11:51:01 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted - Runtime - 0 ms (100.00%), Memory - 19.5 MB (65.40%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.cpp
+++ 0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        unordered_map<int,int> m;
-        for(int i=0;i<n;i++) {
-            int complement=target-nums[i];
-            if(m.count(complement)) return {min(i,m[complement])+1,max(i,m[complement])+1};
-            m[nums[i]]=i;
+        int l=0,r=n-1;
+        while(l<=r) {
+            if(nums[l]+nums[r]==target) return{l+1,r+1};
+            else if(nums[l]+nums[r]<target) l++;
+            else r--;
         }
         return {};
     }

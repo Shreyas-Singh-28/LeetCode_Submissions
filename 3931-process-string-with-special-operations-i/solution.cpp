05d00117ff4eb2a15851250bf056f05d015a714e
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-06-18 11:29:27 +0000

    Sync LeetCode submission - Process String with Special Operations I - Runtime - 6 ms (38.92%), Memory - 39.6 MB (92.76%)

---
+++ 3931-process-string-with-special-operations-i/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans="";
+        for(auto &ch:s) {
+            if(ch=='*') {
+                if(!ans.empty()) ans.pop_back();
+            }
+            else if(ch=='#') {
+                ans=ans+ans;
+            }
+            else if(ch=='%') {
+                reverse(ans.begin(),ans.end());
+            }
+            else{
+                ans.push_back(ch);
+            }
+        }
+        return ans;
+    }
+};

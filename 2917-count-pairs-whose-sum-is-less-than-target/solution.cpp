edeb48e0b9418afb08d3729c884c301606133763
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-07-02 17:21:38 +0000

    Sync LeetCode submission - Count Pairs Whose Sum is Less than Target - Runtime - 0 ms (100.00%), Memory - 24.6 MB (52.14%)

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int res=0;
+        int n=static_cast<int>(nums.size());
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(nums[i]+nums[j]<target) res++;
+            }
+        }
+        return res;
+    }
+};

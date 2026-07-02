c1bc2c3cf38b859aae3d58929747c24180d9e752
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-07-02 17:18:07 +0000

    Sync LeetCode submission - Find the Maximum Number of Elements in Subset - Runtime - 65 ms (80.05%), Memory - 122.8 MB (29.03%)

--- 3299-find-the-maximum-number-of-elements-in-subset/solution.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
+        int res=1;
         unordered_map<long long,int> mp;
         for(auto &ele:nums) mp[ele]++;
-
-        int res=1;
         if(mp.count(1)) res=max(res,(mp[1]%2==0)?mp[1]-1:mp[1]);
-
         for(auto &[key,value]:mp) {
             if(key==1) continue;
             int elements=0;
             long long temp=key;
-            for(;mp.count(temp) && mp[temp]>=2;temp*=temp) {
+            while(mp.count(temp) && mp[temp]>=2) {
                 elements+=2;
+                temp*=temp;
             }
-            if(mp.count(temp)) elements++;
+            if(mp.count(temp) && mp[temp]==1) elements++;
             else elements--;
             res=max(res,elements);
         }

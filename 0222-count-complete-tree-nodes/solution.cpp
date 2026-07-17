20f41f671beca4170d2e7cac776210bd94428b9a
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-07-17 19:17:00 +0000

    Sync LeetCode submission - Count Complete Tree Nodes - Runtime - 3 ms (3.91%), Memory - 31.3 MB (45.07%)

---
+++ 0222-count-complete-tree-nodes/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int total_nodes=0;
+    void dfs(TreeNode *root) {
+        total_nodes++;
+        if(root->left) dfs(root->left);
+        if(root->right) dfs(root->right);
+    }
+    int countNodes(TreeNode* root) {
+        if(root==nullptr) return 0;
+        dfs(root);
+        return total_nodes;
+    }
+};

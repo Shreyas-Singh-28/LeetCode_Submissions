2f1bf36b469286f67b8475b25370c0229edc0257
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-07-23 22:11:40 +0000

    Sync LeetCode submission - Next Greater Element I - Runtime - 3 ms (40.70%), Memory - 13 MB (15.72%)

--- 0496-next-greater-element-i/solution.cpp
+++ 0496-next-greater-element-i/solution.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        unordered_map<int,int> m;
-        stack<int> s;
-        for(auto num:nums2) {
-            while(!s.empty() && s.top()<num) {
-                m[s.top()]=num;
-                s.pop();
+        stack<int> st;
+        int n=static_cast<int>(nums2.size());
+        int len=static_cast<int>(nums1.size());
+        vector<int> ans(len);
+        unordered_map<int,int> mp;
+        for(int i=n-1;i>=0;i--) {
+            while(!st.empty() && nums2[i]>=nums2[st.top()]) {
+                st.pop();
             }
-            s.push(num);
+            if(!st.empty()) {
+                // res[i]=st.top();
+                mp[nums2[i]]=st.top();
+            }
+            st.push(i);
         }
-        // while(!s.empty()) {
-        //     m[s.top()]=-1;
-        //     s.pop();
-        // }
-        for(auto num:nums1) {
-            ans.push_back(m.count(num)?m[num]:-1);
+        for(int i=0;i<len;i++) {
+            if(mp.count(nums1[i])) {
+                ans[i]=nums2[mp[nums1[i]]];
+            }
+            else ans[i]=-1;
         }
         return ans;
     }

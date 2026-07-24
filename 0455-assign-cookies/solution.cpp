4a6987adb81d8a448cd3ca8db1930cc4b24b2358
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-07-24 09:51:14 +0000

    Sync LeetCode submission - Assign Cookies - Runtime - 4 ms (86.12%), Memory - 44.8 MB (48.72%)

--- 0455-assign-cookies/solution.cpp
+++ 0455-assign-cookies/solution.cpp
@@ -5,12 +5,15 @@ public:
         sort(s.begin(),s.end());
         int i=0,j=0;
         int res=0;
-        while(i<g.size() && j<s.size()) {
-            if(s[j]>=g[i]) {
-                res++;
-                i++;
+        int n=static_cast<int>(g.size());
+        int len=static_cast<int>(s.size());
+        for(int i=0;i<n;i++) {
+            while(j<len && g[i]>s[j]) {
+                j++;
             }
+            if(j==len) break;
             j++;
+            res++;
         }
         return res;
     }

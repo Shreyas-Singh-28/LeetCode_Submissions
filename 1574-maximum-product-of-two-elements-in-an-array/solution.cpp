043a0938aa32f92e7f24264f8754e817e44fbda4
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-07-27 05:47:30 +0000

    Sync LeetCode submission - Maximum Product of Two Elements in an Array - Runtime - 0 ms (100.00%), Memory - 13.5 MB (20.16%)

--- 1574-maximum-product-of-two-elements-in-an-array/solution.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/solution.cpp
@@ -2,16 +2,7 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=static_cast<int>(nums.size());
-        int l1=-1,l2=-1;
-        for(int i=0;i<n;i++) {
-            if(nums[i]>=l1) {
-                l2=l1;
-                l1=nums[i];
-            }
-            else if(nums[i]<l1 && nums[i]>l2) {
-                l2=nums[i];
-            }
-        }
-        return (l1-1)*(l2-1);
+        sort(nums.begin(),nums.end());
+        return (nums[n-1]-1)*(nums[n-2]-1);
     }
 };

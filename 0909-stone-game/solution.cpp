cd908e018540763c12e005f271cab21596889673
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-02 13:05:25 +0000

    Sync LeetCode submission - Stone Game - Runtime - 0 ms (100.00%), Memory - 10.6 MB (49.31%)

---
+++ 0909-stone-game/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // alice forces bob to choose either only odd or even indexed elements (which has a smaller sum)
+        return true;
+    }
+};

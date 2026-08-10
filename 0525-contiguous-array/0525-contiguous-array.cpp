fa245ddb9d3a0a4cb11a391b49941919079c5598
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-10 20:04:37 +0530

    Time: 20 ms (80.14%), Space: 88.2 MB (73.21%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -6,27 +6,16 @@ public:
         // 0 -1 0 1 2 3 4 5 4 3 2
         // 0 -1 0 -1 0
         int n=static_cast<int>(nums.size());
-        vector<int> pref(n+1);
-        pref[0]=0;
-        int summ=0;
-        for(int i=0;i<n;i++) {
-            if(nums[i]==0) summ-=1;
-            else summ+=1;
-            pref[i+1]=summ;
-        }
         unordered_map<int,int> mp;
+        mp[0]=-1;
         int res=0;
-        for(int i=0;i<n+1;i++) {
-            if(!mp.count(pref[i])) mp[pref[i]]=i;
-            else {
-                res=max(res,i-mp[pref[i]]);
-                // mp[pref[i]]=i;
-            }
+        int pref=0;
+        for(int i=0;i<n;i++) {
+            if(nums[i]==0) pref-=1;
+            else pref+=1;
+            if(!mp.count(pref)) mp[pref]=i;
+            else res=max(res,i-mp[pref]);
         }
-        // for(int i=n;i>=0;i--) if(!mp2.count(pref[i])) mp2[pref[i]]=i;
-        // for(int i=0;i<n+1;i++) {
-        //     res=max(res,mp2[pref[i]]-mp1[pref[i]]);
-        // }
         return res;
     }
 };

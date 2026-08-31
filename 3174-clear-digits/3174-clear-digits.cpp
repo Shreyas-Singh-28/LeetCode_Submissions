b83a8f1cd0f73b68079dd1c88102e1a50d4017af
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-09-01 01:00:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.59%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    // cb345a
     string clearDigits(string s) {
-        stack<char> st;
-        int n=s.length();
-        for(int i=n-1;i>=0;i--) {
-            if(!st.empty() && isalpha(s[i]) && isdigit(st.top())) st.pop();
-            else st.push(s[i]);
-        }
         string res="";
-        while(!st.empty()) {
-            char ch=st.top();
-            res=res+ch;
-            st.pop();
+        for(char &ch:s) {
+            if(isdigit(ch) && !res.empty() && isalpha(res.back())) res.pop_back();
+            else res.push_back(ch);
         }
         return res;
     }
